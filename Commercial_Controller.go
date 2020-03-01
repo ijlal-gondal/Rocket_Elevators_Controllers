2bcdc0f0291190513a996e2d3bab130507618463
Ijlal Gondal <ijlal.gondal@ontariotechu.net>
2020-03-01 18:58:32 -0500

    sixteenth commit

--- Commercial_Controller.go
+++ Commercial_Controller.go
@@ -12,11 +12,67 @@ func main() {
 	// fmt.Println("Hello Sam!")
 	battery := Battery{floorTotal: 66, basementTotal: 6, columnTotal: 4, elevatorPerColumn: 5}
 	battery.NewBattery()
-	fmt.Println(battery)
-	// fmt.Println("myke", battery.columnList[0])
+	// fmt.Println(battery)
 
-	var requestedFloor = 2
-	battery.assignElevator(requestedFloor)
+	// Scenario 1
+
+	battery.columnList[1].elevatorList[0].direction = "DOWN"
+	battery.columnList[1].elevatorList[0].floor = 20
+	battery.columnList[1].elevatorList[1].direction = "UP"
+	battery.columnList[1].elevatorList[1].floor = 3
+	battery.columnList[1].elevatorList[2].direction = "DOWN"
+	battery.columnList[1].elevatorList[2].floor = 13
+	battery.columnList[1].elevatorList[3].direction = "DOWN"
+	battery.columnList[1].elevatorList[3].floor = 15
+	battery.columnList[1].elevatorList[4].direction = "DOWN"
+	battery.columnList[1].elevatorList[4].floor = 6
+	var requestedFloor = 20
+	battery.AssignElevator(requestedFloor)
+
+	//Scenario 2
+
+	// battery.columnList[2].elevatorList[0].direction = "UP"
+	// battery.columnList[2].elevatorList[0].floor = 1
+	// battery.columnList[2].elevatorList[0].floorRequestList = append(battery.columnList[2].elevatorList[0].floorRequestList, 21)
+	// battery.columnList[2].elevatorList[1].direction = "UP"
+	// battery.columnList[2].elevatorList[1].floor = 23
+	// battery.columnList[2].elevatorList[2].direction = "DOWN"
+	// battery.columnList[2].elevatorList[2].floor = 33
+	// battery.columnList[2].elevatorList[3].direction = "DOWN"
+	// battery.columnList[2].elevatorList[3].floor = 40
+	// battery.columnList[2].elevatorList[4].direction = "UP"
+	// battery.columnList[2].elevatorList[4].floor = 39
+	// var requestedFloor = 36
+	// battery.AssignElevator(requestedFloor)
+
+	//Scenario 3
+	// battery.columnList[3].elevatorList[0].direction = "DOWN"
+	// battery.columnList[3].elevatorList[0].floor = 58
+	// battery.columnList[3].elevatorList[1].direction = "UP"
+	// battery.columnList[3].elevatorList[1].floor = 50
+	// battery.columnList[3].elevatorList[2].direction = "UP"
+	// battery.columnList[3].elevatorList[2].floor = 46
+	// battery.columnList[3].elevatorList[3].direction = "UP"
+	// battery.columnList[3].elevatorList[3].floor = 1
+	// battery.columnList[3].elevatorList[4].direction = "DOWN"
+	// battery.columnList[3].elevatorList[4].floor = 60
+	// var floorNumber = 54
+	// battery.RequestElevator(floorNumber)
+
+	//Scenario 4
+
+	// battery.columnList[0].elevatorList[0].direction = "IDLE"
+	// battery.columnList[0].elevatorList[0].floor = -4
+	// battery.columnList[0].elevatorList[1].direction = "IDLE"
+	// battery.columnList[0].elevatorList[1].floor = 1
+	// battery.columnList[0].elevatorList[2].direction = "DOWN"
+	// battery.columnList[0].elevatorList[2].floor = -3
+	// battery.columnList[0].elevatorList[3].direction = "UP"
+	// battery.columnList[0].elevatorList[3].floor = -6
+	// battery.columnList[0].elevatorList[4].direction = "DOWN"
+	// battery.columnList[0].elevatorList[4].floor = -1
+	// var floorNumber = -3
+	// battery.RequestElevator(floorNumber)
 
 }
 
@@ -44,16 +100,11 @@ type Column struct {
 	floorList     []int
 }
 
-// func createElevator()
-
 func (b *Battery) NewBattery() {
-	// b := Battery{floorTotal: floorTotal}
-	// b.basementTotal = basementTotal
-	// b.columnTotal = columnTotal
-	// b.elevatorPerColumn = elevatorPerColumn
+
 	floorExcludingBasement := b.floorTotal - b.basementTotal
 	avgFloorPerColumn := int(math.Floor(float64((floorExcludingBasement / (b.columnTotal - 1)))))
-	fmt.Println("avgFloorPerColumn", avgFloorPerColumn)
+	// fmt.Println("avgFloorPerColumn", avgFloorPerColumn)
 	startFloor := 2
 	endFloor := avgFloorPerColumn
 	currentColumn := 1
@@ -63,21 +114,17 @@ func (b *Battery) NewBattery() {
 			column := Column{ID: currentColumn, elevatorTotal: b.elevatorPerColumn, startFloor: -b.basementTotal, endFloor: -1}
 			b.columnList = append(b.columnList, column)
 
-			column.floorList = append(column.floorList, 1)
-			for i := column.endFloor; i >= column.startFloor; i-- {
-				column.floorList = append(column.floorList, i)
+			b.columnList[currentColumn-1].floorList = append(b.columnList[currentColumn-1].floorList, 1)
+			for i := b.columnList[currentColumn-1].endFloor; i >= b.columnList[currentColumn-1].startFloor; i-- {
+				b.columnList[currentColumn-1].floorList = append(b.columnList[currentColumn-1].floorList, i)
 			}
-			fmt.Println("this is floorlist", column.floorList)
-			// fmt.Println(string.Join(" | ", floorList));
+			fmt.Println("this is floorlist", b.columnList[currentColumn-1].floorList)
 
 			for i := 1; i <= b.elevatorPerColumn; i++ {
 				elevator := Elevator{ID: i}
-				column.elevatorList = append(column.elevatorList, elevator)
-				// fmt.Println(column.elevatorList[i-1].ID + " | ")
+				b.columnList[currentColumn-1].elevatorList = append(b.columnList[currentColumn-1].elevatorList, elevator)
 
 			}
-			fmt.Println("this is elevator list", column.elevatorList[0])
-			// fmt.Println("this is how columns looks like",)
 
 		} else {
 			column := Column{ID: currentColumn, elevatorTotal: b.elevatorPerColumn, startFloor: startFloor, endFloor: endFloor}
@@ -85,21 +132,17 @@ func (b *Battery) NewBattery() {
 			startFloor = endFloor + 1
 			endFloor = endFloor + avgFloorPerColumn
 
-			column.floorList = append(column.floorList, 1)
-			for i := column.startFloor; i <= column.endFloor; i++ {
-				column.floorList = append(column.floorList, i)
+			b.columnList[currentColumn-1].floorList = append(b.columnList[currentColumn-1].floorList, 1)
+			for i := b.columnList[currentColumn-1].startFloor; i <= b.columnList[currentColumn-1].endFloor; i++ {
+				b.columnList[currentColumn-1].floorList = append(b.columnList[currentColumn-1].floorList, i)
 			}
-			fmt.Println("this is floorlist", column.floorList)
-			// fmt.Println(string.Join(" | ", floorList));
+			fmt.Println("this is floorlist", b.columnList[currentColumn-1].floorList)
 
 			for i := 1; i <= b.elevatorPerColumn; i++ {
 				elevator := Elevator{ID: i}
-				column.elevatorList = append(column.elevatorList, elevator)
-				// fmt.Println(column.elevatorList[i-1].ID + " | ")
+				b.columnList[currentColumn-1].elevatorList = append(b.columnList[currentColumn-1].elevatorList, elevator)
 
 			}
-			fmt.Println("this is elevatorlist", column.elevatorList[0])
-			// fmt.Println("\n")
 
 		}
 		currentColumn++
@@ -108,11 +151,8 @@ func (b *Battery) NewBattery() {
 
 }
 
-func (b *Battery) assignElevator(requestedFloor int) *Column {
+func (b *Battery) AssignElevator(requestedFloor int) *Column {
 	var bestColumn Column
-	// elevator := Elevator{ID: 1}
-	// b.columnList[0].elevatorList = append(b.columnList[0].elevatorList, elevator)
-	fmt.Println("columnList test marc", b.columnList[0].elevatorList[0])
 
 	for i, eachColumn := range b.columnList {
 		if requestedFloor <= b.columnList[i].endFloor && requestedFloor >= b.columnList[i].startFloor {
@@ -128,25 +168,26 @@ func (b *Battery) assignElevator(requestedFloor int) *Column {
 		userDirection := "DOWN"
 		userFloor := 1
 		foundElevator := bestColumn.findElevator(userDirection, requestedFloor, userFloor)
-		fmt.Println("Best elevator for current scenario is: {0} \n", foundElevator.ID)
+		fmt.Println("Best elevator for current scenario is: ", foundElevator.ID)
 	} else {
 		userDirection := "UP"
 		userFloor := 1
 		foundElevator := bestColumn.findElevator(userDirection, requestedFloor, userFloor)
-		fmt.Println("Best elevator for current scenario is: {0} \n", foundElevator.ID)
+		fmt.Println("Best elevator for current scenario is: ", foundElevator.ID)
 	}
 
 	return &bestColumn
 }
 func (bestColumn *Column) findElevator(userDirection string, requestedFloor, userFloor int) Elevator {
 	fmt.Println("userDirection is", userDirection)
+	fmt.Println("userFloor  is", userFloor)
 	elevatorSubList := []Elevator{}
-	fmt.Println("bestColumn.elevatorList", len(bestColumn.elevatorList))
+	// fmt.Println("bestColumn.elevatorList", len(bestColumn.elevatorList))
 	for i := 0; i < len(bestColumn.elevatorList); i++ {
 
-		fmt.Println("elevator ID {0}", bestColumn.elevatorList[i].ID)
-		fmt.Println("elevator floor {0}", bestColumn.elevatorList[i].floor)
-		fmt.Println("elevator direction {0}", bestColumn.elevatorList[i].direction)
+		// fmt.Println("elevator ID {0}", bestColumn.elevatorList[i].ID)
+		// fmt.Println("elevator floor {0}", bestColumn.elevatorList[i].floor)
+		// fmt.Println("elevator direction {0}", bestColumn.elevatorList[i].direction)
 
 		if userFloor < bestColumn.elevatorList[i].floor && userDirection != bestColumn.elevatorList[i].direction {
 			elevatorSubList = append(elevatorSubList, bestColumn.elevatorList[i])
@@ -159,114 +200,76 @@ func (bestColumn *Column) findElevator(userDirection string, requestedFloor, use
 
 	closestElevator := elevatorSubList[0]
 	closeness := 10
-	// System.Console.WriteLine(elevatorSubList[0].ID);
 
 	for i := 0; i < len(elevatorSubList); i++ {
 		{
 			// System.Console.WriteLine("userFloor: {0}", userFloor);
 			// System.Console.WriteLine("elevator.floor: {0}", elevator.floor);
-			if int(math.Abs(float64(userFloor-bestColumn.elevatorList[i].floor))) < closeness {
-				closeness = int(math.Abs(float64(userFloor - bestColumn.elevatorList[i].floor)))
+			if int(math.Abs(float64(userFloor-elevatorSubList[i].floor))) < closeness {
+				closeness = int(math.Abs(float64(userFloor - elevatorSubList[i].floor)))
 				closestElevator = elevatorSubList[i]
 			}
 
 		}
-		// System.Console.WriteLine("closest elevator: {0}", closestElevator.ID);
 
 	}
 	return closestElevator
 }
 
-// 	return selectedColumn
+func (b *Battery) RequestElevator(floorNumber int) *Column {
+	var bestColumn Column
 
-// }
+	for i, eachColumn := range b.columnList {
+		if floorNumber <= b.columnList[i].endFloor && floorNumber >= b.columnList[i].startFloor {
+			bestColumn = eachColumn
+		}
+	}
 
-// bestColumn := b.columnList.Where(c.floorList.Contains(requestedFloor)).FirstOrDefault();
-// fmt.Println("best Column for your requested floor is: {0} \n", bestColumn.ID);
-// floorNumber, requestedFloor int
+	fmt.Println("best Column for your requested floor is: ", bestColumn.ID)
 
-// bestCol := Column{}
+	// sets user direction for top or basement floors
+	if bestColumn.ID == 1 {
+		userDirection := "UP"
+		userDestination := 1
+		foundElevator := bestColumn.returnElevator(userDirection, floorNumber, userDestination)
+		fmt.Println("Best elevator for current scenario is: ", foundElevator.ID)
+	} else {
+		userDirection := "DOWN"
+		userDestination := 1
+		foundElevator := bestColumn.returnElevator(userDirection, floorNumber, userDestination)
+		fmt.Println("Best elevator for current scenario is: ", foundElevator.ID)
+	}
 
-// for i := 0; i < len(b.columnList); i++ {
+	return &bestColumn
+}
+func (bestColumn *Column) returnElevator(userDirection string, floorNumber, userDestination int) Elevator {
+	fmt.Println("userDirection is", userDirection)
+	elevatorSubList := []Elevator{}
+	// fmt.Println("bestColumn.elevatorList", len(bestColumn.elevatorList))
+	for i := 0; i < len(bestColumn.elevatorList); i++ {
 
-// 	for _, k := range b.columnList[i].floorList {
+		if userDestination < bestColumn.elevatorList[i].floor && userDirection == bestColumn.elevatorList[i].direction {
+			elevatorSubList = append(elevatorSubList, bestColumn.elevatorList[i])
+		} else if userDestination > bestColumn.elevatorList[i].floor && userDirection == bestColumn.elevatorList[i].direction {
+			elevatorSubList = append(elevatorSubList, bestColumn.elevatorList[i])
 
-// 		if floorNumber == 1 && requestedFloor == k {
-// 			bestCol := b.columnList[i]
-// 			// bestCol.findElevator(floorNumber, requestedFloor)
-// 			return &bestCol
+		}
 
-// 		} else if floorNumber == k && requestedFloor == 1 {
-// 			bestCol := b.columnList[i]
-// 			// bestCol.findElevator(floorNumber, requestedFloor)
-// 			return &bestCol
-// 		}
-// 	}
-// }
-// // bestCol.findElevator(floorNumber, requestedFloor)
-// fmt.Println("bestCol.ID", bestCol.ID)
-// return &bestCol
+	}
 
-// func(c *NewBattery) createElevatorAndFloor() {
+	closestElevator := elevatorSubList[0]
+	closeness := 10
 
-// }
+	for i := 0; i < len(elevatorSubList); i++ {
+		{
 
-// func createFloors(startFloor int, endFloor int) *Elevator {
-// 	if startFloor < 0 {
+			if int(math.Abs(float64(userDestination-elevatorSubList[i].floor))) < closeness {
+				closeness = int(math.Abs(float64(userDestination - elevatorSubList[i].floor)))
+				closestElevator = elevatorSubList[i]
+			}
 
-// 		floorList = append(floorList, 1)
-// 		for i := startFloor; i >= endFloor; i-- {
-// 			floorList.append(i)
-// 		}
-// 		fmt.Println()("Floor List")
-// 		fmt.Println(string.Join(" | ", floorList))
+		}
 
-// 		fmt.Println()("Elevator List")
-// 		for i := 1; i <= elevatorTotal; i++ {
-// 			elevatorList.append(Elevator(i, 1))
-// 			System.Console.Write(elevatorList[i-1].ID + " | ")
-
-// 		}
-// 		fmt.Println()("\n")
-// 	}
-
-// }
-// b := Battery{basementTotal: basementTotal}
-// b := Battery{columnTotal: columnTotal}
-// b := Battery{elevatorPerColumn: elevatorPerColumn}
-
-// fmt.Println(currentColumn)
-// }
-// if startFloor < 0 {
-// 	floorList.append(1)
-// 	for i := startFloor; i >= endFloor; i-- {
-// 		floorList.append(floorList, i)
-// 	}
-// 	fmt.Println("Floor List")
-// 	fmt.Println(string.Join(" | ", floorList))
-
-// 	fmt.Println("Elevator List")
-// 	for i := 1; i <= elevatorTotal; i++ {
-// 		elevatorList.append(elevatorList, Elevator(i, 1))
-// 		System.Console.Write(elevatorList[i-1].ID + " | ")
-
-// 	}
-// 	fmt.Println("\n")
-
-// } else {
-
-// 	floorList.append(1)
-// 	for i := startFloor; i <= endFloor; i++ {
-// 		floorList.append(i)
-
-// 	}
-// 	fmt.Println("Floor List")
-// 	fmt.Println(string.Join(" | ", floorList))
-
-// 	fmt.Println("Elevator List")
-// 	for i = 1; i <= elevatorTotal; i++ {
-// 		elevatorList.append(elevatorList, Elevator(i, 1))
-// 		System.Console.Write(elevatorList[i-1].ID + " | ")
-
-// 	}
-// fmt.Println("\n")
+	}
+	return closestElevator
+}
